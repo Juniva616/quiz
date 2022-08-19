5535ecae3f52e6d811670e6b75ac51b40697f53d
Juniva616 <juniva616@gmail.com>
2022-08-19 23:51:33 +0000

    Add background image to footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
 alert("Are you ready to test your knowledge?  Make your choice and press the button 'True' if you think it is true or 'False' if you think it is not a true statement. Let's START! ");
 
-const quiz = {
-    statement: '  Male lions do 90 percent of the hunting. ',
-    correctAnswer: 'true',
+const quizArray = {
+    statement: 'Male lions do 90 percent of the hunting.',
+    correctAnswer: 'false',
     moreInfo: {
         imgFile: 'url(../assets/images/lions.jpg)',
         textInfo: ' Lionesses, not male lions, do the majority of hunting for their pride. Lionesses hunt around 90 percent of the time, while the males protect their pride."', 
-    }
+    },
 }
 
-addEventListener(this: Window, Event: 'click');
-document.getElementById('question').innerText = quiz(statement);
+document.getElementById('question').innerText = quizArray.statement;
