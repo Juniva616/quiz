b26147a4b449f90c1e0074b8287e4e9c710292dd
Juniva616 <juniva616@gmail.com>
2022-08-19 23:16:08 +0000

    Add style to flexbox items and code to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
- 
+alert("Are you ready to test your knowledge?  Make your choice and press the button 'True' if you think it is true or 'False' if you think it is not a true statement. Let's START! ");
+
+const quiz = {
+    statement: '  Male lions do 90 percent of the hunting. ',
+    correctAnswer: 'true',
+    moreInfo: {
+        imgFile: 'url(../assets/images/lions.jpg)',
+        textInfo: ' Lionesses, not male lions, do the majority of hunting for their pride. Lionesses hunt around 90 percent of the time, while the males protect their pride."', 
+    }
+}
+
+addEventListener(this: Window, Event: 'click');
+document.getElementById('question').innerText = quiz(statement);
